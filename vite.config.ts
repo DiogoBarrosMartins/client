6dc455d5841b56a24156eea6374120303624729a
DiogoBarrosMartins <diogobarrosmartins@hotmail.com>
2025-09-15 00:06:35 +0100

    net

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // agora podes usar "@/components/..."
+    },
+  },
   server: {
     port: 5173,
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // onde o NestJS corre
+        target: "http://localhost:3000", // backend local NestJS
         changeOrigin: true,
       },
     },
